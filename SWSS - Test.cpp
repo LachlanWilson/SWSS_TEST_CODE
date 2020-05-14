913bc8206809b4fc59fe429743751aa2c277d372
LachlanWilson <49221904+LachlanWilson@users.noreply.github.com>
2020-05-14 15:04:41 +1000

    setup

---
+++ SWSS - Test.cpp
@@ -0,0 +1,52 @@
+// SWSS - C++.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+int Sensor() {
+  //NIR Readings
+
+  int R = sensor.getR();
+  int S = sensor.getS();
+  int T = sensor.getT();
+  int U = sensor.getU();
+  int V = sensor.getV();
+  int W = sensor.getW();
+
+  int SensorData[6] = {R,S,T,U,V,W};
+  
+  return SensorData;
+}
+
+void MechElements(int SensorInfo) {
+  if (SensorInfo == 0){
+    //allow any material to pass, this is undefined material
+  } else if (SensorInfo == 1){
+    //sort into material 1
+  } else if (SensorInfo == 2){
+    //sort into material 2
+  } else if (SensorInfo == 3){
+    //sort into material 3
+  } else if (SensorInfo == 4){
+    //sort into material 4
+  } 
+  // many of these could be made depending on number of known materials
+}
+
+int DataInterpret(int sensorData[6]) {
+  if (((sensorData[0] > 0)&&(sensorData[0] < 100))&&((sensorData[1] > 0)&&(sensorData[1] < 100))){
+      // example
+    return 1;
+  }else if (sensorData[1] > 100){
+    // nested if statements will be used also here, so that we can check for very specific conditions
+    return 2;
+  }
+  return 0; // no material or unknown material
+}
+
+
+void loop(){
+  while (true) {
+    int sensorData = Sensor(); // return number
+    int SensorInterp = DataInterpret(sensorData);
+    MechElements(SensorInterp);
+  }
+}
